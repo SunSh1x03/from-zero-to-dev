615b8a4183a044404c2011c7dd4a4d16cca24153
Filipe Penido <filipe.penido@blacklabel.team>
2025-10-21 21:52:18 +0000

    Modified Vercel

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,11 @@
 function Home() {
-    return <h1>Para construtir uma boa casa é preciso ter uma boa base.</h1>
+  return (
+    <>
+      <h1>Para construir uma boa casa é preciso ter uma boa base.</h1>
+      <br />
+      <h1>Testando Vercel</h1>
+    </>
+  );
 }
 
 export default Home;
