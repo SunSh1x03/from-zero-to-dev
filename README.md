b13c4daee82bc9be8ef55abb8809b6b3ce20d401
Filipe Penido <filipe.penido@blacklabel.team>
2025-10-22 00:55:27 +0000

    Add scripts `lint:check` and `lint:fix`

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # from-zero-to-dev
+
 Repositório com meus estudos e projetos do curso.dev do Filipe Deschamps — foco em práticas reais de desenvolvimento, mentalidade profissional e construção de sistemas completos do zero.
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,13 @@
 function Home() {
-    return <h1>Para construtir uma boa casa é preciso ter uma boa base.</h1>
+  return <h1>Para construtir uma boa casa é preciso ter uma boa base.</h1>;
+}
+
+function teste() {
+  console.log("teste");
+}
+
+function teste() {
+  console.log("indentaçao errada");
 }
 
 export default Home;
