db53b56dacd0cdf2637a038a83db7478f4a3f992
Filipe Penido <filipe.penido@blacklabel.team>
2025-10-24 19:36:00 +0000

    Fix `jsconfig.json` file

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -3,7 +3,7 @@ import database from "infra/database.js";
 async function status(request, response) {
   const result = await database.query("SELECT 1 + 1 as sum;");
   console.log(result.rows);
-  response.status(200).json({ chave: "Testando status" });
+  response.status(200).json({ chave: "Testando status!" });
 }
 
 export default status;
