324c313c591d6b6be358493453d8801347f657ad
Filipe Penido <filipe.penido@blacklabel.team>
2025-10-25 15:05:45 +0000

    Finish `/api/v1/status` endpoint

--- tests/unit/calculadora.test.js
+++ tests/unit/calculadora.test.js
@@ -1,38 +1,40 @@
-test("nome do teste", callbackFuction);
+// Os testes iniciais foram desabilitados para não poluir o terminal
+
+test.skip("nome do teste", callbackFuction);
 
 function callbackFuction() {
   console.log("Esta função esta sendo chamada?");
 }
 
-test("nome do teste", function () {
+test.skip("nome do teste", function () {
   console.log("E assim funciona?");
 });
 
-test("nome do teste", () => {
+test.skip("nome do teste", () => {
   console.log("e agora?");
 });
 
-test("testando outra condição do meu sistema", () => {
+test.skip("testando outra condição do meu sistema", () => {
   console.log("testando");
 });
 
-test("Espero que 1 seja 1", () => {
+test.skip("Espero que 1 seja 1", () => {
   expect(1).toBe(1);
 });
 
 const calculadora = require("../../models/calculadora.js");
 
-test("somar 2 + 2 deveria retornar 4", () => {
+test.skip("somar 2 + 2 deveria retornar 4", () => {
   const resultado = calculadora.somar(2, 2);
   expect(resultado).toBe(4);
 });
 
-test("somar 5 + 100 deveria retornar 105", () => {
+test.skip("somar 5 + 100 deveria retornar 105", () => {
   const resultado = calculadora.somar(5, 100);
   expect(resultado).toBe(105);
 });
 
-test("somar 'banana' + 100 deveria retornar 'Erro'", () => {
+test.skip("somar 'banana' + 100 deveria retornar 'Erro'", () => {
   const resultado = calculadora.somar("banana", 100);
   expect(resultado).toBe("Erro");
 });
